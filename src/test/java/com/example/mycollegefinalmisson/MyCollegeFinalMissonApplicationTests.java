4ff3e33cd50d11d5f66d4654bd41252cc6efa68c
Big wenhao <www.niewenhao2001@qq.com>
2024-02-11 19:04:37 +0800

    Day 01 build the base project

---
+++ src/test/java/com/example/mycollegefinalmisson/MyCollegeFinalMissonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mycollegefinalmisson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCollegeFinalMissonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
